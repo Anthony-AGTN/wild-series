6fe1bebba9627adf0d52c4dc5a1442151900405b
Anthony GOUTON <anthony.gouton.wcs@gmail.com>
2022-05-23 21:31:05 +0200

    add Route for program

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -4,9 +4,11 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class ProgramController extends AbstractController
 {
+    #[Route('/program/', name: 'program_index')]
     public function index(): Response
     {
         return new Response(
