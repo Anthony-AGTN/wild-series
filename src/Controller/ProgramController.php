1cadddd6f03db26b03d9c8b372a2ef00d57a07bb
Anthony Gouton <anthony.gouton.wcs@gmail.com>
2022-05-25 15:35:37 +0200

    update wild-series

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -6,13 +6,23 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/program', name: 'program_')]
 class ProgramController extends AbstractController
 {
-    #[Route('/program/', name: 'program_index')]
+    #[Route('/', name: 'index')]
     public function index(): Response
     {
         return $this->render('program/index.html.twig', [
             'website' => 'Wild Series',
+        ]);
+    }
+
+    #[Route('/{id}', methods: ['GET'], requirements: ['id'=>'\d+'], name: 'show')]
+    public function show($id): Response
+    {
+        return $this->render('program/show.html.twig', [
+            'website' => 'Wild Series With ID',
+            'id' => $id,
         ]);
     }
 }
