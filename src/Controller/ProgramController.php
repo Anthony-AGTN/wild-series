698d9683abf821cf6ab9a54874b88fb1211710ad
Anthony Gouton <anthony.gouton.wcs@gmail.com>
2022-05-25 15:40:46 +0200

    update wild-series

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -21,7 +21,7 @@ class ProgramController extends AbstractController
     public function show($id): Response
     {
         return $this->render('program/show.html.twig', [
-            'website' => 'Wild Series With ID',
+            'website' => 'Wild Series with ID',
             'id' => $id,
         ]);
     }
