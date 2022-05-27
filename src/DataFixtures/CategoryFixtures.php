c27f5798c08ce19b627bc7910c7935750bf28b57
Anthony GOUTON <anthony.gouton.wcs@gmail.com>
2022-05-27 22:23:37 +0200

    modif on CATEGORIES lopp for categoryName in CategoryFixtures

--- src/DataFixtures/CategoryFixtures.php
+++ src/DataFixtures/CategoryFixtures.php
@@ -18,7 +18,7 @@ class CategoryFixtures extends Fixture
 
     public function load(ObjectManager $manager)
     {
-        foreach (self::CATEGORIES as $key => $categoryName) {
+        foreach (self::CATEGORIES as $categoryName) {
             $category = new Category();
             $category->setName($categoryName);
             $manager->persist($category);
