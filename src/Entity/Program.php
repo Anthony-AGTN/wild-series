94501387c38cc55fecfdf1812802c533782b2eac
Anthony Gouton <anthony.gouton.wcs@gmail.com>
2022-05-30 11:12:38 +0200

    add new relation ManyToOne to Program and Category entitys

--- src/Entity/Program.php
+++ src/Entity/Program.php
@@ -22,6 +22,10 @@ class Program
     #[ORM\Column(type: 'string', length: 255, nullable: true)]
     private $poster;
 
+    #[ORM\ManyToOne(targetEntity: Category::class)]
+    #[ORM\JoinColumn(nullable: false)]
+    private $category;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -59,6 +63,18 @@ class Program
     public function setPoster(?string $poster): self
     {
         $this->poster = $poster;
+
+        return $this;
+    }
+
+    public function getCategory(): ?Category
+    {
+        return $this->category;
+    }
+
+    public function setCategory(?Category $category): self
+    {
+        $this->category = $category;
 
         return $this;
     }
