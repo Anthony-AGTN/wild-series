e0c13ddcc119408ee62f6729782c0ac6b86c8f12
Anthony GOUTON <anthony.gouton.wcs@gmail.com>
2022-05-30 23:23:38 +0200

    add findAll for index program

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -2,6 +2,7 @@
 // src/Controller/ProgramController.php
 namespace App\Controller;
 
+use App\Repository\ProgramRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,14 +11,18 @@ use Symfony\Component\Routing\Annotation\Route;
 class ProgramController extends AbstractController
 {
     #[Route('/', name: 'index')]
-    public function index(): Response
+    public function index(ProgramRepository $programRepository): Response
     {
+        $programs = $programRepository->findAll();
+
+
         return $this->render('program/index.html.twig', [
             'website' => 'Wild Series',
+            'programs' => $programs,
         ]);
     }
 
-    #[Route('/{id}', methods: ['GET'], requirements: ['id'=>'\d+'], name: 'show')]
+    #[Route('/{id}', name: 'show', methods: ['GET'], requirements: ['id'=>'\d+'])]
     public function show($id): Response
     {
         return $this->render('program/show.html.twig', [
