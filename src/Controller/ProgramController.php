c7586e964c22d169dc4507cacb27723a2fc57ec2
Anthony GOUTON <anthony.gouton.wcs@gmail.com>
2022-06-14 23:21:32 +0200

    add ProgramType

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -5,9 +5,11 @@ namespace App\Controller;
 use App\Entity\Episode;
 use App\Entity\Program;
 use App\Entity\Season;
+use App\Form\ProgramType;
 use App\Repository\ProgramRepository;
 use App\Repository\SeasonRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -25,12 +27,32 @@ class ProgramController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}', name: 'show', methods: ['GET'], requirements: ['id'=>'\d+'])]
+    #[Route('/new', name: 'new')]
+    public function new(Request $request, ProgramRepository $programRepository): Response
+    {
+        $program = new Program;
+        $form = $this->createForm(ProgramType::class, $program);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted()) {
+            $programRepository->add($program, true);
+
+            // Redirect to categories list
+            return $this->redirectToRoute('program_index');
+        }
+
+        // Render the form
+        return $this->renderForm('program/new.html.twig', [
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'show', methods: ['GET'], requirements: ['id' => '\d+'])]
     public function show(int $id, Program $program): Response
     {
         if (!$program) {
             throw $this->createNotFoundException(
-                'No program with id : '.$id.' found in program\'s table.'
+                'No program with id : ' . $id . ' found in program\'s table.'
             );
         }
 
@@ -42,7 +64,7 @@ class ProgramController extends AbstractController
         ]);
     }
 
-    #[Route('/{program}/seasons/{season}', name: 'season_show', methods: ['GET'], requirements: ['program'=>'\d+', 'season'=>'\d+'])]
+    #[Route('/{program}/seasons/{season}', name: 'season_show', methods: ['GET'], requirements: ['program' => '\d+', 'season' => '\d+'])]
     public function showSeason(Program $program, Season $season): Response
     {
 
@@ -61,7 +83,7 @@ class ProgramController extends AbstractController
         ]);
     }
 
-    #[Route('/{program}/seasons/{season}/episode/{episode}', name: 'episode_show', methods: ['GET'], requirements: ['program'=>'\d+', 'season'=>'\d+', 'episode'=>'\d+'])]
+    #[Route('/{program}/seasons/{season}/episode/{episode}', name: 'episode_show', methods: ['GET'], requirements: ['program' => '\d+', 'season' => '\d+', 'episode' => '\d+'])]
     public function showEpisode(Program $program, Season $season, Episode $episode): Response
     {
 
--- src/Form/ProgramType.php
+++ src/Form/ProgramType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\Program;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,10 +14,10 @@ class ProgramType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
-            ->add('synopsis')
-            ->add('poster')
-            ->add('category')
+            ->add('title', TextType::class)
+            ->add('synopsis', TextareaType::class)
+            ->add('poster', TextType::class)
+            ->add('category', null, ['choice_label' => 'name'])
         ;
     }
 
