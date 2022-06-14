4a2a8ffb13eaab2e46c41bf58c041227572c0280
Anthony GOUTON <anthony.gouton.wcs@gmail.com>
2022-06-14 23:01:19 +0200

    add CategoryType

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -2,9 +2,12 @@
 // src/Controller/ProgramController.php
 namespace App\Controller;
 
+use App\Entity\Category;
+use App\Form\CategoryType;
 use App\Repository\CategoryRepository;
 use App\Repository\ProgramRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -22,6 +25,26 @@ class CategoryController extends AbstractController
         ]);
     }
 
+    #[Route('/new', name: 'new')]
+    public function new(Request $request, CategoryRepository $categoryRepository): Response
+    {
+        $category = new Category();
+
+        $form = $this->createForm(CategoryType::class, $category);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted()) {
+            $categoryRepository->add($category, true);
+
+            return $this->redirectToRoute('category_index');
+        }
+
+        return $this->renderForm('category/new.html.twig', [
+            'form' => $form,
+        ]);
+    }
+
     #[Route('/show/{categoryName}', name: 'show', methods: ['GET'])]
     public function show(string $categoryName, CategoryRepository $categoryRepository, ProgramRepository $programRepository): Response
     {
@@ -29,10 +52,10 @@ class CategoryController extends AbstractController
 
         if (!$category) {
             throw $this->createNotFoundException(
-                'No category with name : '.$categoryName.' found in category\'s table.'
+                'No category with name : ' . $categoryName . ' found in category\'s table.'
             );
         }
-        
+
         $programs = $programRepository->findBy(['category' => $category], ['id' => 'DESC'], 3);
 
         return $this->render('category/show.html.twig', [
