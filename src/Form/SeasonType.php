0016a575335704c44988021f7fc15b7b2c68ff7c
Anthony GOUTON <anthony.gouton.wcs@gmail.com>
2022-06-15 00:21:54 +0200

    add a CRUD for season entity

---
+++ src/Form/SeasonType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Season;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SeasonType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('number', IntegerType::class)
+            ->add('year', IntegerType::class)
+            ->add('description', TextareaType::class)
+            ->add('program', null, ['choice_label' => 'title'])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Season::class,
+        ]);
+    }
+}
